5318d035643852cdf1901f29057e8cd0961bd77d
Benjamin Malin <benjaminamalin@gmail.com>
2017-10-16 14:15:19 -0400

    added import structure to components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,2 +1,5 @@
 import React from 'react';
 // import {connect} from 'react-redux';
+
+import NavBar from '../nav-bar/nav-bar';
+import DrinkOption from '../drink-option/drink-option';
--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,2 +1,8 @@
 import React from 'react';
 // import {connect} from 'react-redux';
+
+import Header from '../header/header';
+import Ingredient from '../ingredient/ingredient';
+import DrinkOption from '../drink-option/drink-option';
+import DrinkDescroption from '../drink-description/drink-description';
+import MenuItem from '../menu-item/menu-item';
