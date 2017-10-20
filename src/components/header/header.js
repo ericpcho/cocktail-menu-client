b33b11a2ae57b8e7c945744398c172e0eaf287fa
ericpcho <ericpcho@gmail.com>
2017-10-20 11:17:27 -0700

    changed text to help the user navigate more easily

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -11,22 +11,22 @@ export class Header extends React.Component {
       if (this.props.view === 'chooseLiquor') {
         headerText = 
           <div>
-            <h2>Welcome!</h2>
-            <h3>Click a liquor below to find a drink</h3>
+            <h2>Cocktail Menu Maker</h2>
+            <h3>Start building your menu by selecting a liquor below</h3>
           </div>
       }
       else if (this.props.view === 'chooseBase') {
         headerText = 
           <div>
             <h2>{this.props.selection}</h2>
-            <h3>Pick your base ingredient</h3>
+            <h3>Select a base ingredient</h3>
           </div>
       }
       else if (this.props.view === 'chooseDrink') {
         headerText = 
           <div>
             <h2>{this.props.selection[0]} › {this.props.selection[1]}</h2>
-            <h3>Select a drink</h3>
+            <h3>View your drink's recipe card</h3>
           </div>
       }
       else if (this.props.view === 'recipe') {
