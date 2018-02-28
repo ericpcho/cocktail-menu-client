078469f56c3a8e2541dfd3d7da1ebcda25b97f49
Ben Malin <benjaminamalin@gmail.com>
2018-02-28 11:37:15 -0500

    finished cleaning src files

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import Page from './components/page/page';
-import SharedMenu from './components/shared-menu/shared-menu'
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import Page from "./components/page/page";
+import SharedMenu from "./components/shared-menu/shared-menu";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function Layout(){
+function Layout() {
   return (
     <Router>
-
-        <div>
-          <Route path="/:id" component={SharedMenu} />
-          <Route exact path="/" component={Page} />
-        </div>
-
+      <div>
+        <Route path="/:id" component={SharedMenu} />
+        <Route exact path="/" component={Page} />
+      </div>
     </Router>
-  )
+  );
 }
 
 export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
-import App from './app.js';
-import {createStore, applyMiddleware} from 'redux';
-import {reducer} from './reducers/reducer.js';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import App from "./app.js";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers/reducer.js";
+import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(reducer, applyMiddleware(thunk));
-
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 registerServiceWorker();
