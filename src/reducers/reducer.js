afa80300f2de9b0494e0944b66d19a1b3a63d811
Ben Malin <benjaminamalin@gmail.com>
2018-02-28 11:36:29 -0500

    cleaned up reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,122 +1,93 @@
-import * as actions from '../actions/cocktail.js'
+import * as actions from "../actions/cocktail.js";
 
 const initialState = {
-    view: 'home',
-    alcohol: ['Whiskey', 'Rum', 'Vodka', 'Gin', 'Tequila'],
-    cocktails: [],
-    selectedCocktail: [],
-    loading: false,
-    selection: '',
-    menuItems: [],
-    error: null,
-    id: ''
+  view: "home",
+  alcohol: ["Whiskey", "Rum", "Vodka", "Gin", "Tequila"],
+  cocktails: [],
+  selectedCocktail: [],
+  loading: false,
+  selection: "",
+  menuItems: [],
+  error: null,
+  id: ""
 };
 
 export const reducer = (state = initialState, action) => {
+  if (action.type === actions.CHANGE_VIEW) {
+    return Object.assign({}, state, {
+      view: action.view
+    });
+  } else if (action.type === actions.FETCH_COCKTAILS_BY_NAME_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    });
+  } else if (action.type === actions.FETCH_COCKTAILS_BY_NAME_SUCCESS) {
+    return Object.assign({}, state, {
+      cocktails: action.cocktails,
+      hasCocktail: false,
+      loading: false
+    });
+  } else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    });
+  } else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_SUCCESS) {
+    return Object.assign({}, state, {
+      view: "chooseBase",
+      loading: false,
+      cocktails: action.cocktails,
+      selection: action.cocktails[0].alcohol
+    });
+  } else if (action.type === actions.FILTER_COCKTAILS_BY_BASE) {
+    return Object.assign({}, state, {
+      view: "chooseDrink",
+      cocktails: action.cocktails,
+      selection: [state.selection, action.selection]
+    });
+  } else if (action.type === actions.DISPLAY_RECIPE) {
+    return Object.assign({}, state, {
+      view: "recipe",
+      selectedCocktail: action.selectedCocktail
+    });
+  } else if (action.type === actions.DISPLAY_SEARCHED_RECIPE) {
+    return Object.assign({}, state, {
+      selectedCocktail: action.selectedCocktail,
+      hasCocktail: true
+    });
+  } else if (action.type === actions.SAVE_TO_MENU) {
+    return Object.assign({}, state, {
+      menuItems: [...state.menuItems, state.selectedCocktail[0]]
+    });
+  } else if (action.type === actions.RETURN_TO_HOME) {
+    return Object.assign({}, state, {
+      view: "home",
+      cocktails: [],
+      selectedCocktail: [],
+      selection: "",
+      hasCocktail: false
+    });
+  } else if (action.type === actions.GO_TO_MENU) {
+    return Object.assign({}, state, {
+      view: "menu"
+    });
+  } else if (action.type === actions.DELETE_MENU_ITEM) {
+    return Object.assign({}, state, {
+      menuItems: action.updatedMenu
+    });
+  } else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  } else if (action.type === actions.POST_MENU_SUCCESS) {
+    return Object.assign({}, state, {
+      id: action.menu._id
+    });
+  } else if (action.type === actions.FETCH_MENU_SUCCESS) {
+    return Object.assign({}, state, {
+      view: "sharedMenu",
+      menuItems: action.menu
+    });
+  }
 
-    if (action.type === actions.CHANGE_VIEW) {
-        return Object.assign({}, state, {
-            view: action.view
-        })
-    }
-
-    else if (action.type === actions.FETCH_COCKTAILS_BY_NAME_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true
-        })
-    }
-
-    else if (action.type === actions.FETCH_COCKTAILS_BY_NAME_SUCCESS) {
-        return Object.assign({}, state, {
-            cocktails: action.cocktails,
-            hasCocktail: false,
-            loading: false
-        })
-    }
-
-    else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_REQUEST) {
-        return Object.assign({}, state, {
-            loading: true
-        })
-    }
-
-    else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_SUCCESS) {
-        return Object.assign({}, state, {
-            view: 'chooseBase',
-            loading: false,
-            cocktails: action.cocktails,
-            selection: action.cocktails[0].alcohol
-        })
-    }
-
-    else if (action.type === actions.FILTER_COCKTAILS_BY_BASE) {
-        return Object.assign({}, state, {
-            view: 'chooseDrink',
-            cocktails: action.cocktails,
-            selection: [state.selection, action.selection]
-        })
-    }
-
-    else if (action.type === actions.DISPLAY_RECIPE) {
-        return Object.assign({}, state, {
-            view: 'recipe',
-            selectedCocktail: action.selectedCocktail,
-        })
-    }
-
-    else if (action.type === actions.DISPLAY_SEARCHED_RECIPE) {
-        return Object.assign({}, state, {
-            selectedCocktail: action.selectedCocktail,
-            hasCocktail: true
-        })
-    }
-
-    else if (action.type === actions.SAVE_TO_MENU) {
-        return Object.assign({}, state, {
-            menuItems: [...state.menuItems, state.selectedCocktail[0]]
-        })
-    }
-
-    else if (action.type === actions.RETURN_TO_HOME) {
-        return Object.assign({}, state, {
-            view: 'home',
-            cocktails: [],
-            selectedCocktail: [],
-            selection: '',
-            hasCocktail: false
-        })
-    }
-
-    else if (action.type === actions.GO_TO_MENU) {
-        return Object.assign({}, state, {
-            view: 'menu',
-        })
-    }
-
-    else if (action.type === actions.DELETE_MENU_ITEM) {
-        return Object.assign({}, state, {
-            menuItems: action.updatedMenu
-        })
-    }
-
-    else if (action.type === actions.FETCH_COCKTAILS_BY_ALCOHOL_ERROR) {
-        return Object.assign({}, state, {
-            error: action.error
-        })
-    }
-
-    else if (action.type === actions.POST_MENU_SUCCESS) {
-        return Object.assign({}, state, {
-            id: action.menu._id
-        })
-    }
-
-    else if (action.type === actions.FETCH_MENU_SUCCESS) {
-        return Object.assign({}, state, {
-            view: "sharedMenu",
-            menuItems: action.menu
-        })
-    }
-
-    return state
-}
+  return state;
+};
--- src/reducers/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import reducer from './reducer'; 
-import * as actions from '../actions/cocktail'; 
-
-// describe('reducer', () => {
-  
-//   it('Should handle fetchCocktailSuccess action')
-
-// })
